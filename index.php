f690988c4fb818415e1e084675d06b4c079355ec
LeonardoDeLaLoza <leonardo.delaloza@devitm.com>
2025-05-28 14:54:36 -0600

    Acavando el html

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<?php
-    while( have_posts()): the_post();
-
-    the_title();
-    the_content();
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     
-endwhile;
-?>
+
+    <main>
+        <?php
+            while( have_posts()): the_post();
+
+            the_title();
+            the_content();
+
+        endwhile;
+        ?>
+
+    </main>
+</body>
+</html>
