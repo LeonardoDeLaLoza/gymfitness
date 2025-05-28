eb86b67890a8949d1bf610f515f826402722dc0d
LeonardoDeLaLoza <leonardo.delaloza@devitm.com>
2025-05-28 16:14:29 -0600

    Se agrego una hoja de estilos desde el php y se agrego el normalize css

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>gymfitness</title>
+    <?php wp_head(); ?>
 </head>
 <body>
     <header class="header">
@@ -11,9 +12,16 @@
             <div class="logo">
                 <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="logo">
             </div>
-            <nav>
-                <!--Nav aqui -->
-            </nav>
+            
+            <?php
+            $args = array(
+                'theme_location' => 'menu-principal',
+                'container' => 'nav',
+                'container_class' => 'menu-principal'
+            );
+            wp_nav_menu()
+            ?>
+
         </div>
     </header>
 
